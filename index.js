da0178ef81130dfba6d88b8db7f164bbbc383a9d
ranamiah221 <ranarasul21@example.com>
2025-04-15 10:44:42 +0600

    create job portal server repo

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT ||5000;
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+// middleware...
+app.use(cors())
+app.use(express.json())
+
+
+
+// Database collection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ungcn7e.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
+    
+    //job portal database collection ...
+    const jobCollections = client.db('jobDB').collection('jobs') 
+
+    // jobs collection...
+    app.get('/jobs', async(req, res)=>{
+        const cursor = await jobCollections.find().toArray()
+        res.send(cursor)
+    })
+
+    app.get('/jobs/:id', async(req, res)=>{
+      const id = req.params.id
+      const query={_id : new ObjectId(id)}
+      const result= await jobCollections.findOne(query)
+      res.send(result)
+    })
+    
+
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res)=>{
+    res.send("Job Portal Server")
+})
+
+app.listen(port, ()=>{
+    console.log(`Job portal server running on port : ${port}`);
+})
